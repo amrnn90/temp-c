e7e11c4a9084606b8aeae6bd0c490bfa20626f46
Amr Noman <amrnn90@gmail.com>
2020-02-21 02:07:01 +0300

    basic translatable experiment

---
+++ app/Article.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Article extends Model
+{
+    use HasTranslations;
+    protected $guarded = [];
+
+    public $translatable = ['title', 'body'];
+}
